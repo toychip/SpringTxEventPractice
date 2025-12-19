1bae4a4dfdf6020f39c9874e55805a48a8bc96da
toychip <dlawnsgud427@naver.com>
2025-12-19 10:05:16 +0900

    feat: 메인 이벤트, 부가 이벤트 엔티티 추가

---
+++ src/main/java/com/stock/txpractice/LogEntity.java
@@ -0,0 +1,19 @@
+package com.stock.txpractice;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class LogEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false)
+    private String id;
+
+    private String content;
+}

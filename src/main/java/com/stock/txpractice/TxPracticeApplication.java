ebc6769fc5a21746cc83c55ab77c0b7407d3e837
toychip <dlawnsgud427@naver.com>
2025-12-19 09:45:22 +0900

    Initial commit

---
+++ src/main/java/com/stock/txpractice/TxPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.stock.txpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TxPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TxPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stock/txpractice/TxPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stock.txpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TxPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
